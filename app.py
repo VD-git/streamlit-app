509f4c1812495165bb58676173d681e8ba2f2047
victordias94 <victordias94@hotmail.com>
2025-07-03 00:27:48 -0300

    ip addr

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import logging
+from logging import getLogger
 
-st.title('Stream Project')
+logger = getLogger()
+if logger.handlers:  # logger is already setup, don't setup again
+    pass
+else:
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.INFO)
 
-st.write('Testing a application of streamlit')
+if __name__ == "__main__":
+    
+    st.title('Stream Project')
+    
+    st.write(f'Addr IP {st.context.ip_address}')
+
+    logger.info(f"ip {st.context.ip_address}")
+
+
