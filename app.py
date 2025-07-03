a6cd95b9e23a38229646808a149301cd52ee750a
victordias94 <victordias94@hotmail.com>
2025-07-03 00:32:38 -0300

    get_remote_ip

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import logging
 from logging import getLogger
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
 
 logger = getLogger()
 if logger.handlers:  # logger is already setup, don't setup again
@@ -9,12 +11,28 @@ else:
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.INFO)
 
+def get_remote_ip() -> str:
+    """Get remote ip."""
+
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+
+    return session_info.request.remote_ip
+
 if __name__ == "__main__":
     
     st.title('Stream Project')
     
-    st.write(f'Addr IP {st.context.ip_address}')
+    st.write(f'Addr IP {get_remote_ip()}')
 
-    logger.info(f"ip {st.context.ip_address}")
+    logger.info(f"ip {get_remote_ip()}")
 
 
