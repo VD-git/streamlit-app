e753a912c50da18a3e5f6b516537148696586289
victordias94 <victordias94@hotmail.com>
2025-07-03 23:04:10 -0300

    id address

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import logging
 from logging import getLogger
-import subprocess
 
 logger = getLogger()
 if logger.handlers:  # logger is already setup, don't setup again
@@ -11,15 +10,15 @@ else:
     logger.setLevel(logging.INFO)
     
 
+
+
 if __name__ == "__main__":
     
     st.title('Stream Project')
 
-    st.write(f"Addr IP {st.context.headers.__dict__}")
+    st.write(f"Addr IP {st.context.headers.__dict__.get('_headers').get('X-Forwarded-For')}")
 
-    st.write(f"Addr IP {subprocess.run(['curl', 'ifconfig.me'])}")
-
-    logger.info(f"ip {st.context.ip_address}")
+    logger.info(f"ip {st.context.headers.__dict__.get('_headers').get('X-Forwarded-For')}")
 
 
 
