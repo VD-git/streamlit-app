bedea011d626456f8f9412b5582466aaa3b006a3
victordias94 <victordias94@hotmail.com>
2025-07-03 22:30:39 -0300

    id address

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import logging
 from logging import getLogger
-from streamlit import runtime
-from streamlit.runtime.scriptrunner import get_script_run_ctx
-from streamlit.web.server.websocket_headers import _get_websocket_headers
+import subprocess
 
 logger = getLogger()
 if logger.handlers:  # logger is already setup, don't setup again
@@ -19,7 +17,7 @@ if __name__ == "__main__":
 
     st.write(f"Addr IP {st.context.headers.__dict__}")
 
-    st.write(f"Addr IP {st.context.ip_address}")
+    st.write(f"Addr IP {subprocess.run(['curl', 'ifconfig.me'])}")
 
     logger.info(f"ip {st.context.ip_address}")
 
