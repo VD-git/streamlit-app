8bd2f82a56edacbb239805632eded6e4efaa5391
victordias94 <victordias94@hotmail.com>
2025-08-25 22:14:51 -0300

    sqlite3

--- Home_Page.py
+++ Home_Page.py
@@ -1,9 +1,9 @@
 # Home.py
 import streamlit as st
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 st.set_page_config(
