daa8194ac950d9f63b3720a95087047b8b610db8
victordias94 <victordias94@hotmail.com>
2025-09-07 20:02:34 -0300

    updating home message with new project

--- Home_Page.py
+++ Home_Page.py
@@ -22,7 +22,19 @@ st.markdown(
     **1. Chatbot With Mistral + MongoDB**  
     An interactive chatbot powered by the [Mistral API](https://mistral.ai/) (`mistral-small-latest`). This chatbot supports real-time conversations and uses [MongoDB](https://www.mongodb.com/) to store chat history securely.  
     > ⚠️ *Note: All messages exchanged with the chatbot are saved in the database.*
+    --------------------------------------------------------------------------------------------------------------------------
 
+    **2. Search Documents PDF**
+    You can upload a PDF and, using the **RAG (Retrieval-Augmented Generation)** architecture, ask questions about its content.
+
+    - **Similarity score**: Adjust how closely the answers match your query:
+      - `0` → Only exact matches are considered. Some queries may return nothing.
+      - `2` → Considers all possible matches.
+      - **Default:** `1.5` → Balances accuracy and coverage.
+
+    - The system uses **GPT-4o-mini** for chat, retrieving information from **ChromaDB**.
+    > ⚠️ *Note: All uploaded PDFs are stored in the Chroma database. You can remove later your collection if needed.*
+    --------------------------------------------------------------------------------------------------------------------------
     """
 )
 
