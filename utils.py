047cb87534a864a13c32855539fa8fa255aa5d17
VD-git <victordias94@gmail.com>
2025-10-04 22:40:37 -0300

    fixing pokemon class

--- utils.py
+++ utils.py
@@ -368,7 +368,8 @@ class PokemonAgent:
             max_tokens=4096,
             top_p=1.0
         )
-        self.model_with_tools = self.llm.bind_tools([self.identify_pokemon])
+        self.model_with_tools_identify = self.llm.bind_tools([self.identify_pokemon])
+        self.model_with_tools_exists = self.llm.bind_tools([self.pokemon_exists])
         self.POKEMON = None
         self.config = {"configurable": {"thread_id": "single_session_memory"}}
         self.app = self.creation_of_graph_workflow()
@@ -393,7 +394,7 @@ class PokemonAgent:
             azure_endpoint = st.secrets["azure"]["azure_endpoint"],
             api_version = st.secrets["azure"]["api_version"],
             azure_deployment = "gpt-4o-mini",
-            temperature=1.0,
+            temperature=0.2,
             max_tokens=4096,
             top_p=1.0
         )
@@ -403,7 +404,7 @@ class PokemonAgent:
                 content=(
                     """
                     You are an AI Assistant and your function is to identified which is the pokemon that is based on the description given.
-                    In case it is not possible to identify reply exactly the following between triple brackets: ```Not able to identified```, just reply its name, nothing more"""
+                    In case it is not possible to identify reply exactly the following between triple backticks: ```Not able to identified```, just reply its name, nothing more"""
                 )
             ),
             HumanMessage(content=f"Discover which one is my pokemon based on the description. Description: {description}")
@@ -411,61 +412,147 @@ class PokemonAgent:
         response = llm.invoke(messages)
         return response.content
 
+    @staticmethod
+    @tool
+    def pokemon_exists(pokemon_name: str) -> bool:
+        """
+        Checks whether a given Pokémon exists in the PokéAPI database.
+    
+        Sends a GET request to the PokéAPI using the provided Pokémon name.
+        Returns True if the Pokémon exists (HTTP 200 response), otherwise returns False.
+    
+        Args:
+            pokemon_name (str): The name of the Pokémon to check.
+    
+        Returns:
+            bool: True if the Pokémon exists in the database, False otherwise.
+    
+        Example:
+            >>> pokemon_exists("pikachu")
+            True
+    
+            >>> pokemon_exists("fakepokemon123")
+            False
+        """
+        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}")
+    
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+
     def retrieve_last_human_message(self, state: MessagesState):
-        """Function that retrieves the las human message"""
+        """Function that retrieves the last human message"""
         return [msg for msg in state["messages"] if isinstance(msg, HumanMessage)][-1].content
     
     def retrieve_pokemon_ai_message(self, state: MessagesState):
-        return [msg for msg in state["messages"] if isinstance(msg, AIMessage)][-1].content
+        """Function that returns the last pokemon guesser"""
+        # Here it has -2 because I will always check if the pokemon exists before trying to guess, being always last second tool
+        return [msg for msg in state["messages"] if isinstance(msg, ToolMessage)][-2].content
+
+    def response_guide_node(self, state: MessagesState):
+        return {"messages": [AIMessage(content="Here you should provide info so I can find your pokemon")]}
     
     def call_initial_discover_pokemon(self, state: MessagesState):
         SYSTEM_PROMPT = """
         You are an AI Assistant and your function is to identified which is the pokemon that is based on the description given.
-        In case it is not possible to identify which pokemon exactly, or you are not a 100 % sure, reply exactly the following between triple brackets: ```Not able to identified```.
+        Case the pokemon is not explicitable said you can use de tool identify_pokemon in order to help you find it.
+        In case it is not possible to identify which pokemon exactly, or you are not a 100 % sure, reply exactly the following between triple backticks: ```Not able to identified```.
         When is sure of the pokemon, just reply its name, nothing more
         """
-        last_message = state["messages"]
+        print("CHATBOT")
+        last_messages, last_message = state["messages"], state["messages"][-1]
     
-        messages = [SystemMessage(content=SYSTEM_PROMPT)] + last_message
+        messages = [SystemMessage(content=SYSTEM_PROMPT)] + last_messages
 
+        # When pokemon is already identified and also confirmed
         if self.POKEMON is not None:
             return {"messages": [AIMessage(content=f"Pokemon already discovered here: {self.POKEMON}. Reset the page if it's wanted another search.")]}
-        elif isinstance(last_message, AIMessage) and last_message.tool_calls:
-            return {"messages": [AIMessage(content=last_message.tool_calls[0]["response"])]}
+        # Response considering the feedback from the tool
+        elif isinstance(last_message, ToolMessage):
+            if last_message.content == "```Not able to identified```":
+                return {"messages": [AIMessage(content="Please, provide more info to identify the pokemon")]}
+            else:
+                return {"messages": [AIMessage(content=f"Pokemon: {last_message.content}")]}
+        # Based on the human message i need to call a llm in order to have the possibility to call the tools
         else:
-            return {"messages": [self.model_with_tools.invoke(messages)]}
+            return {"messages": [self.model_with_tools_identify.invoke(messages)]}
     
     def confirm_keep_discover_pokemon(self, state: MessagesState):
-        last_message = state["messages"][-1]
+        print("EVALUATOR")
+        SYSTEM_PROMPT = """
+        You are an AI Assistant and your function is to identified if the pokemon that is being passed exists into the database from PokeAPI.
+        Tool that will be used is pokemon_exists and will return a boolean weather exists or not.
+        In case tool returns False, reply exactly the following between triple backticks:```Pokémon does not exist```
+        In case tool returns True, reply just the name of the pokemon.
+        """
+        last_messages, last_message = state["messages"], state["messages"][-1]
     
-        if (isinstance(last_message, ToolMessage)) and (last_message.content == "```Not able to identified```"):
-            return {"messages": [AIMessage(content="Please, provide more info to identify the pokemon")]}
-        elif self.retrieve_last_human_message(state).strip().lower() in ["yes", "y"]:
+        messages = [SystemMessage(content=SYSTEM_PROMPT)] + [last_message]
+        
+        last_message = state["messages"][-1]
+
+        # Evaluating weather the pokemon found before actually exists inside the database
+        if isinstance(last_message, ToolMessage):
+            pokemon = self.retrieve_pokemon_ai_message(state)
+            if last_message.content == "true":
+                return {"messages": [AIMessage(content=f"Is your pokemon {pokemon}? Reply with **yes/y** if it is, or help giving more info about it :).")]}
+            else:
+                return {"messages": [AIMessage(content=f"Pokemon {pokemon} does not exist in PokeAPI database, try another one, please")]}
+        elif self.retrieve_last_human_message(state).strip().lower() in ["yes", "y", "yep", "yeah"]:
             self.POKEMON = self.retrieve_pokemon_ai_message(state)
             return {"messages": [AIMessage(content=f"Great! We discovered your Pokémon! {self.POKEMON}")]}
+        # Based on the human message i need to call a llm in order to have the possibility to call the tools
         else:
-            return {"messages": [AIMessage(content=f"Is your pokemon {last_message.content}? Reply with **yes** if it is, or help giving more info about it :).")]}
-    
-    
-    def should_continue(self, state: MessagesState):
-        if self.POKEMON is not None:
+            return {"messages": [self.model_with_tools_exists.invoke(messages)]}
+
+    def should_continue_guesser(self, state: MessagesState):
+        print("GUESSER")
+        last_message = state["messages"][-1]
+
+        # Checking if a tool will be called
+        if last_message.tool_calls:
+            return "tools_pokemon_guesser"
+        # Response from the tools or if a tool wasn't called (guide_node) for that
+        else:
+            # Option 1 from the tool
+            if last_message.content == "Please, provide more info to identify the pokemon":
+                return END
+            # Option 2 from the tool
+            elif ("Pokemon:" in last_message.content) or (self.retrieve_last_human_message(state).strip().lower() in ["yes", "y", "yep", "yeah"]):
+                return "evaluator"
+            # Option if the tool wasn't even called
+            else:
+                return "guide_node"
+
+    def should_continue_checker(self, state: MessagesState):
+        print("CHECKER")
+        last_message = state["messages"][-1]
+
+        # Achieving this edge it is already sure that you have a pokemon to check
+        if last_message.tool_calls:
+            return "tools_pokemon_checker"
+        else:
             return END
-        else:
-            return "tools"
 
     def creation_of_graph_workflow(self):
 
-        tool_node = ToolNode(tools = [self.identify_pokemon])
+        tool_node_pokemon_guesser = ToolNode(tools = [self.identify_pokemon])
+        tool_node_pokemon_checker = ToolNode(tools = [self.pokemon_exists])
         workflow = StateGraph(MessagesState)
     
         workflow.add_node("chatbot", self.call_initial_discover_pokemon)
         workflow.add_node("evaluator", self.confirm_keep_discover_pokemon)
-        workflow.add_node("tools", tool_node)
+        workflow.add_node("tools_pokemon_guesser", tool_node_pokemon_guesser)
+        workflow.add_node("tools_pokemon_checker", tool_node_pokemon_checker)
+        workflow.add_node("guide_node", self.response_guide_node)
         
         workflow.add_edge(START, "chatbot")
-        workflow.add_conditional_edges("chatbot", self.should_continue, [END, "tools"])
-        workflow.add_edge("tools", "evaluator")
-        workflow.add_edge("evaluator", END)
+        workflow.add_conditional_edges("chatbot", self.should_continue_guesser, ["tools_pokemon_guesser", END, "evaluator", "guide_node"])
+        workflow.add_edge("tools_pokemon_guesser", "chatbot")
+        workflow.add_edge("guide_node", END)
+        workflow.add_conditional_edges("evaluator", self.should_continue_checker, ["tools_pokemon_checker", END])
+        workflow.add_edge("tools_pokemon_checker", "evaluator")
         
         memory = MemorySaver()
         app = workflow.compile(checkpointer = memory)
